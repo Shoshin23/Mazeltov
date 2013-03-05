8f29431c25cb1fcb620057b011c41203285bbb87
Karthik Kannan <karthikk.314@gmail.com>
2013-03-06 04:45:33 +0530

    worked around the routes needed by various parts of the app.

---
+++ config/routes.js
@@ -0,0 +1,71 @@
+var mongoose = require('mongoose')
+  , Article = mongoose.model('Article')
+  , User = mongoose.model('User')
+  , async = require('async')
+
+module.exports = function(app,passport,auth) {
+	//User routes
+var users = require('../app/controllers/users') //adhering to the MVC model. 
+
+app.get('/login',users.login)
+
+app.get('/signup',users.signup)
+
+app.get('/logout',users.logout)
+
+app.post('/users',users.create)
+
+app.post('/users/session',passport,authenticate('local',{failureRedirect: '/login',failureFlash: 'Invaild email or password.'}),users.session)
+
+app.get('/users/:userId',users.show)
+
+app.get('/auth/facebook',passport.authenticate('facebook',{ scope: ['email','user_about_me'],failureRedirect: '/login'}),users.signin)
+
+app.get('/auth/facebook/callback',passport.authenticate('facebook',{failureRedirect:'/login'}),users.authCallback)
+
+app.get('/auth/github',passport.authenticate('github', {failureRedirect: '/login'}), users.signin)
+
+app.get('/auth/github/callback',passport.authenticate('github', {failureRedirect: '/login' }),users.authCallback)
+
+app.get('/auth/twitter',passport.authenticate('twitter', {failureRedirect: '/loin'}). users.signin)
+
+app.get('/auth/twitter/callback',passport.authenticate('twitter', {failureRedirect: '/login'}), users.authCallback)
+
+app.get('/auth/google',passport.authenticate('google', { failureRedirect: ':/login', scope: 'https://www.google.com/m8/feeds'}), users.signin)
+
+app.get('/auth/google/callback',passport.authenticate('google', {failureRedirect: '/login', scope: 'https://www.google.com/m8/feeds'}), users.authCallback)
+
+app.param('userId', users.user)
+
+
+//routes for post
+
+var articles = require('../app/controllers/articles')
+app.get('/articles', articles.index)
+
+app.get('/articles/new',auth.requiresLogin, articles.new)
+
+app.get('/articles',auth.requiresLogin,articles.create)
+
+app.get('/articles/:id',articles.show)
+
+app.get('/articles/:id/edit',auth.requiresLogin,auth.article.hasAuthorization,articles.edit)
+
+app.put('/articles/:id',auth.requiresLogin,auth.article.hasAuthorization,articles.update)
+
+app.del('/articles/:id',auth.requiresLogin,auth.article.hasAuthorization,articles.destroy)
+
+app.param('id',articles.article)
+
+// home 
+app.get('/',articles.index)
+
+//comments
+var comments = require('../app/controllers/comments')
+app.post('/articles/:id/comments',auth.requiresLogin,comments.create)
+
+//tags
+var tags = require('../app/controllers/tags')
+app.get('/tags/:tag',tags.index)
+
+
