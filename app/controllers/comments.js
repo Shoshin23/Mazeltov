8282bd629760b913626d893b6efbe49ba2ff4a74
Karthik Kannan <karthikk.314@gmail.com>
2013-03-06 23:48:26 +0530

    controller for comments. as of now only creating comments.

---
+++ app/controllers/comments.js
@@ -0,0 +1,24 @@
+//This is the controller for comments
+
+var mongoose =  require('mongoose')
+
+//Create a comment
+//
+exports.create = function(req,res) P
+var article = req.article
+var user = req.user
+
+if(!req.body.body) return res.redirect('/articles/'+article.id)
+
+article.comments.push({
+	body:req.body.body,
+	user: user._id
+})
+
+article.save(function(err) {
+	if(err) return res.render('500')
+	res.render('/articles/'+article.id)
+})
+}
+
+
