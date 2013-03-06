9f83e9615e32e19d66145ea435787a2b161f3607
Karthik Kannan <karthikk.314@gmail.com>
2013-03-07 02:56:09 +0530

    added basic user functions like creating a user, display profiles and find by id

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -37,4 +37,49 @@ exports.logout = funcion(req,res) {
 	res.redirect('/login')
 }
 
+//The Session
+
+exports.session = function(req,res) {
+	res.redirect('/')
+}
+
+//Create a user
+
+exports.create = function(req,res) {
+	var user = new User(req.body)
+		user.provider = 'local'
+		user.save(function(err) {
+			if(err) {
+				return res.render('users/signup',{errors:err.errors,user:user})
+			}
+	req.logIn(user,function(err) {
+		if(err) return next(err)
+		return res.redirect('/')
+	})
+		})
+}
+
+//Show user's profile
+
+exports.show = function(req,res) {
+	var user = req.profile
+		res.render('users/show', {
+			title: user.name
+			user: user
+		})
+}
+
+//Find user by id
+//
+exports.user = function(req,res,next,id) {
+	User
+		.findOne({_id : id)}
+		.exec(function (err,user) {
+			if(err) return next(err)
+			if(!user) return next(new Error('Failed to load user' + id))
+			req.profile = user
+			next(
+				})
+			}
+
 
