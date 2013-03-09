1f41f34387603423f7977b895f8599174cee4d8e
Karthik Kannan <karthikk.314@gmail.com>
2013-03-10 02:58:34 +0530

    done with the twitter strategy

--- config/passport.js
+++ config/passport.js
@@ -17,4 +17,60 @@ module.exports = function (passport, config) {
 	})
 
 	passport.deserializeUser(function(id, done) {
+		User.findOne({_id: id}, function (err, user) {
+			done(err,user) 
+		})
+	})
+
+	//use local strategy
+	//
+	passport.use(new LocalStrategy ({
+		usernameField: 'email',
+		passwordField: 'password'
+	},
+	function(email, password,done) {
+		User.findOne({ email: email}), function(err,user) {
+			if(err) {return done(err) }
+			if(!user) {
+				return done(null, false, {message: 'Unknown user'})
+			}
+			if(!user.authenticate(password)) {
+				return done(null, false, {message: 'Invalid password'})
+			}
+			return done(null, user)
+		})
+	}
+	})
+
+//twitter startegy 
+//
+passport.use(new TwitterStrategy({
+	consumerKey: config.twitter.clientID,
+	consumerSecret: config.twitter.clientSecret,
+	callbackURL: config.twitter.callbackURL
+},
+function(token,tokenSecret,profile,done) {
+	User.findOne({'twitter.id': profile.id}, function(err,user) {
+		if(err) { return done(err)}
+		if(!user) {
+			user = new User({
+				name: profile.displayName,
+				username: profile.username,
+				provider: 'twitter',
+				twitter: profile._json
+			})
+			user.save(function (err) {
+				if(err) console.log(err) 
+				return done(err,user)
+			})
+		}
+		else {
+			return done(err,user)
+		}
+	})
+
+}
+)) //dirty braces!
+
+
 
